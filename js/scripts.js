2c36746a8e919e00b4cc9607f4e69a27310a4740
saadad <ferneyz80@gmail.com>
2018-05-22 17:33:03 -0500

    archivos iniciales de prueba
    archivos de la clase css, html y js

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*window.addEventListener('load', function(){
+    document.getElementById('btnVer').addEventListener('click', function(){
+        alert('Botón...');
+    });
+});
+
+function btnClick(){
+    alert('Otro click...');
+}*/
+
+/*let var1 = "valor cambiable";
+console.log(var1);
+
+var1 = "valor cambiado";
+console.log(var1);
+
+const var2 = "valor no cambiable";
+console.log(var2);
+
+var2 = "Intento de cambio de valor";
+console.log(var2);*/
+
+let var1 = 1;
+console.log(var1);
+
+var1 = "Hoola";
+console.log(var1);
+
+var1 = false;
+console.log(var1);
+
+var1 = 1.2;
+console.log(var1);
+
+var1 = [1,2,3,4,5];
+console.log(var1);
+console.log(var1[1]);
